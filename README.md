f4f81de1002720dba2e06b11d8634948bba5745b
Abhishek V Raman <abhishekvraman@gmail.com>
2022-07-03 12:43:40 +0530

    Adding first few files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# py-and-id
+#### Piping and Instrumentation diagram but it's Python Code.
---
+++ src/py-and-id/settings.py
@@ -0,0 +1,5 @@
+class Settings(object):
+    unit_system = "SI"
+    pipe_dp_method = "Clamond"
+    Darcy = True
+    compression_process = "Adiabatic" # Polytropic or Adiabatic/Isentropic
