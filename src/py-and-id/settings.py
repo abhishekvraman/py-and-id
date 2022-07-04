62ad8209cf08c1cd654916806db671e05ab8cdfa
Abhishek V Raman <abhishekvraman@gmail.com>
2022-07-04 22:44:39 +0530

    minor updated.

--- src/py-and-id/settings.py
+++
@@ -1,5 +0,0 @@
-class Settings(object):
-    unit_system = "SI"
-    pipe_dp_method = "Clamond"
-    Darcy = True
-    compression_process = "Adiabatic" # Polytropic or Adiabatic/Isentropic
---
+++ src/py_and_id/__init__.py
@@ -0,0 +1,1 @@
+from py_and_id import settings
